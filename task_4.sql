812ece5bdf942c4e4787cdb2cc040d95954d1a9c
Allan Gori <ogollogori@gmail.com>
2024-10-28 23:30:09 +0300

    Updated sql code in task_4.sql file

--- task_4.sql
+++ task_4.sql
@@ -1,12 +1,13 @@
-USE alx_book_store;
-
-SELECT COLUMN_TYPE, 
-       DATA_TYPE, 
-       IS_NULLABLE, 
-       COLUMN_KEY, 
-       COLUMN_DEFAULT, 
-       EXTRA
+sql_query = """
+SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store' 
-  AND TABLE_NAME = 'Books'
-ORDER BY ORDINAL_POSITION;
+WHERE TABLE_SCHEMA = 'your_database_name' AND TABLE_NAME = 'books';
+"""
+
+file_name = 'task_4.sql'
+
+with open(file_name, 'w') as file:
+    file.write(sql_query)
+
+print(f'SQL query written to {file_name}')
+
