42c104a9ab8668488342abc6050e5cc5bd291fea
Allan Gori <ogollogori@gmail.com>
2024-10-28 10:02:03 +0300

    spelling correction on Except error

--- MySQLServer.py
+++ MySQLServer.py
@@ -19,7 +19,7 @@ try:
         cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store;")
         print("Database 'alx_book_store' created successfully!")
 
-except Error as e:
+except mysql.connect.Error as e:
     print("Error while connecting to MySQL:", e)
 
 finally:
