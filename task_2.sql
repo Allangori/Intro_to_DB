aadeb7ad1fa3032d9f594547e23866003e2c635d
Allan Gori <ogollogori@gmail.com>
2024-10-28 23:44:59 +0300

    Spelling corrections and data type correction in Order_Details Table

--- task_2.sql
+++ task_2.sql
@@ -36,7 +36,7 @@ CREATE TABLE Order_details (
     order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
-    quantity INT NOT NULL,
+    quantity DOUBLE,
     price DECIMAL(10, 2),
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
