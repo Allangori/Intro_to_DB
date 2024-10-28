ec80b89252d16374b26769a933a41697d7158453
Allan Gori <ogollogori@gmail.com>
2024-10-28 23:39:00 +0300

    spelling updates on Table names

--- task_2.sql
+++ task_2.sql
@@ -18,8 +18,8 @@ CREATE TABLE Books (
 
 CREATE TABLE Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) UNIQUE NOT NULL,
     phone VARCHAR(20),
     address TEXT
 );
